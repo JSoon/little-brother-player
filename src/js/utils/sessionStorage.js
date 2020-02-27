2eb4c1c3d88bf3b42f3b094b98ab93a24ee0553f
Soon <serdeemail@gmail.com>
2020-02-27 17:13:27 +0800

    1. Optimize test page 2. Add sessionStorage util (in progress)

---
+++ src/js/utils/sessionStorage.js
@@ -0,0 +1,19 @@
+/**
+ * @description Manipulate browser session storage for player states consistent storage.
+ * 
+ * https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API
+ * 
+ */
+
+const sessionStorage = () => {
+
+  const API = {
+
+  }
+
+
+  return API
+
+}
+
+export default sessionStorage()
